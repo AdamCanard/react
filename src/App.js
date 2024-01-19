14af40905b011dfd55fcb4302839f7c241cdc1b5
JeremyCCox <93951833+JeremyCCox@users.noreply.github.com>
2024-01-18 18:34:20 -0700

    Updated App.js and added .idea to gitignore

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import {useEffect, useRef, useState} from "react";
+
 function App(){
+
+    console.log("hello")
+
     return(
-        <p>
+        <div>
             Hello World
-        </p>
+            <br/>
+            <BoolTest/>
+        </div>
     )
 }export default App;
+
+export function BoolTest() {
+    const [bool, setBool] = useState(false)
+    const [diffBool, setDiffBool] = useState(false)
+    const boolRef = useRef();
+    console.log("booltest")
+
+
+    return (
+        <>
+            {bool ? "True" : "False"}
+            <br/>
+            <input type={"button"} onClick={() => {
+                setBool(!bool)
+            }}/>
+            <input type={"text"} ref={boolRef}/>
+        </>
+    )
+}
